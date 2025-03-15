b06ba62091111cf86b6ea4deea7ac6b2c0502b96
lucifer <3616266449@qq.com>
2025-03-15 23:26:39 +0800

    Auto commit: 2025-03-15 23:26:39

--- docs/.vitepress/theme/components/Artalk.vue
+++ docs/.vitepress/theme/components/Artalk.vue
@@ -3,27 +3,12 @@ import { onMounted } from 'vue'
 import Artalk from 'artalk'
 
 onMounted(() => {
-  // 测试 HTTP 是否可访问
-  fetch('https://xtech-comment.liuqiao.top:8085/api/v2/conf', {
-    mode: 'cors' // 允许跨域
+  Artalk.init({
+    el: '#comments',
+    server: 'https://xtech-comment.liuqiao.top', // ✅ 现在是 HTTPS 了
+    site: 'My VitePress Blog',
+    pageKey: location.pathname,
+    pageTitle: document.title,
   })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Artalk 配置:', data)
-      Artalk.init({
-        el: '#comments',
-        server: 'https://xtech-comment.liuqiao.top:8085/', // HTTP 地址
-        site: 'My VitePress Blog',
-        pageKey: location.pathname, // 文章唯一 ID
-        pageTitle: document.title, // 页面标题
-      })
-    })
-    .catch(error => {
-      console.error('Artalk API 访问失败，请检查 CORS 设置:', error)
-    })
 })
 </script>
-
-<template>
-  <div id="comments"></div>
-</template>
