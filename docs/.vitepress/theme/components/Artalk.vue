c531185b48d4767f1a12b2aaba7fac5bbf62b77e
lucifer <3616266449@qq.com>
2025-03-15 23:43:14 +0800

    Auto commit: 2025-03-15 23:43:14

--- docs/.vitepress/theme/components/Artalk.vue
+++ docs/.vitepress/theme/components/Artalk.vue
@@ -1,14 +1,25 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, nextTick } from 'vue'
 import Artalk from 'artalk'
 
 onMounted(() => {
-  Artalk.init({
-    el: '#comments',
-    server: 'https://xtech-comment.liuqiao.top', // ✅ 现在是 HTTPS 了
-    site: 'My VitePress Blog',
-    pageKey: location.pathname,
-    pageTitle: document.title,
+  nextTick(() => { // ✅ 确保 DOM 渲染完成后再初始化 Artalk
+    const commentEl = document.querySelector('#comments')
+    if (commentEl) {
+      Artalk.init({
+        el: '#comments',  // 绑定评论区
+        server: 'https://xtech-comment.liuqiao.top',  // API 地址
+        site: 'My VitePress Blog',
+        pageKey: location.pathname,  // 当前页面唯一 ID
+        pageTitle: document.title,  // 页面标题
+      })
+    } else {
+      console.error('❌ Error: #comments element not found!')
+    }
   })
 })
 </script>
+
+<template>
+  <div id="comments"></div>
+</template>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,9 @@
-import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import Artalk from './components/Artalk.vue'
 
 export default {
   ...DefaultTheme,
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'doc-after': () => h(Artalk) // 在每篇文章底部显示评论
-    })
+  enhanceApp({ app }) {
+    app.component('Artalk', Artalk) // ✅ 让 Markdown 里可以用 <Artalk />
   }
 }
